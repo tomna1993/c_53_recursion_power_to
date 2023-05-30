73445b3972ec2f8a84234ac568f77c3d67ffd3e7
TomasNagy <tomi.nagy@outlook.com>
2023-05-30 10:10:28 +0200

    refactor(var): missing b letter in exponent_number

--- recursion_power_to.c
+++ recursion_power_to.c
@@ -6,11 +6,11 @@ int calculate_power_to(int base, int exponent);
 int main(void)
 {
     int base_number = get_int("Base: ");
-    int exponent_numer = get_int("Exponent: ");
+    int exponent_number = get_int("Exponent: ");
     
-    int power_of = calculate_power_to(base_number, exponent_numer);
+    int power_of = calculate_power_to(base_number, exponent_number);
 
-    printf ("%i^%i= %i\n", base_number, exponent_numer, power_of);
+    printf ("%i^%i= %i\n", base_number, exponent_number, power_of);
     
     return 0;
 }
